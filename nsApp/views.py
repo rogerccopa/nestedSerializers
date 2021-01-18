4b6b7db503a0422df53b416fc4beb704e0f98527
Roger Ccopa <rogerccopa@gmail.com>
2021-01-17 19:06:06 -0500

    Authentication & authorization implemented

--- nsApp/views.py
+++ nsApp/views.py
@@ -2,9 +2,17 @@ from nsApp.models import Author, Book
 from nsApp.serializers import AuthorSerializer, BookSerializer
 from rest_framework import generics
 
+# commented because we use authentication at a global level (defined inside settings.py)
+# from rest_framework.authentication import BasicAuthentication
+# from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
+
 class AuthorList(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    
+    # set authentication, authorization per class (at class level, not globally)
+    # authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
